495f0c4ff3c7911b4e610e446dbef5a4500f228f
Deepak Yadav <dkyadav.org@gmail.com>
2018-05-23 14:19:18 +0530

    changes so far

--- ex1/star.py
+++ ex1/star.py
@@ -1,22 +1,51 @@
-nos = int(input("Enter number of star:"))
-maxn = nos
-numeroftower = (int(nos/2)) + 1
-stepall = numeroftower
-numberofstar = 1
-stepnum = 0
 
-while numeroftower > 0:
-	toprint = ''
-	#print(numeroftower,'total star:',numberofstar)
+nos = int(input("Enter number of star:"))
+
+if nos%2 == 1:
+
 	maxn = nos
-	while maxn > 0:
-		if maxn>stepall+stepnum or maxn<stepall-stepnum:
-			toprint = toprint + ' '
-		else:
-			toprint = toprint + '*'
-		maxn-=1
-	
-	numberofstar=numberofstar+2
-	numeroftower=numeroftower-1
-	stepnum = stepnum+1
-	print(toprint)
+	numeroftower = (int(nos/2)) + 1
+	stepall = numeroftower
+	#numberofstar = 1
+	stepnum = 0
+
+	while numeroftower > 0:
+		toprint = ''
+		#print(numeroftower,'total star:',numberofstar)
+		maxn = nos
+		while maxn > 0:
+			if maxn>stepall+stepnum or maxn<stepall-stepnum:
+				toprint = toprint + ' '
+			else:
+				toprint = toprint + '*'
+			maxn-=1
+		
+		#numberofstar=numberofstar+2
+		numeroftower=numeroftower-1
+		stepnum = stepnum+1
+		print(toprint)
+
+else:
+
+	maxn = nos + nos-1
+	numeroftower = nos
+	stepall = numeroftower
+	#numberofstar = 1
+	stepnum = 0
+
+	while numeroftower > 0:
+		toprint = ''
+		#print(numeroftower,'total star:',numberofstar)
+		maxn = nos
+		while maxn > 0:
+			if maxn>stepall+stepnum or maxn<stepall-stepnum:
+				toprint = toprint + ' '
+			else:
+				toprint = toprint + '*'
+			maxn-=1
+		
+		#numberofstar=numberofstar+2
+		numeroftower=numeroftower-1
+		stepnum = stepnum+1
+		print(toprint)
+	print('even')
